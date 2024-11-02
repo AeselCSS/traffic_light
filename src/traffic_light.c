ff81c3e8637a761013ed0d715f6a764b0e2191a9
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-11-02 10:01:42 +0100

    Adjusted traffic light control logic in main function to have a step after all red, where red and yellow are lit in one direction while red is lit in the other.

--- src/traffic_light.c
+++ src/traffic_light.c
@@ -5,6 +5,14 @@ int main() {
     SET_TRAFFIC_LIGHT_PINS_OUTPUT();
 
     while (1) {
+        // All red (North/South and East/West)
+        MY_PORT_B = (MY_PORT_B & ~(NS_YELLOW | NS_GREEN | EW_YELLOW | EW_GREEN)) | (NS_RED | EW_RED);
+        DELAY(1); // Delay for ~1 second
+
+        // North/South red, North/South yellow, East/West red
+        MY_PORT_B = (MY_PORT_B & ~(NS_GREEN | EW_YELLOW | EW_GREEN)) | (NS_RED | NS_YELLOW | EW_RED);
+        DELAY(1); // Delay for ~1 second
+
         // North/South green, East/West red
         MY_PORT_B = (MY_PORT_B & ~(NS_RED | NS_YELLOW | EW_YELLOW | EW_GREEN)) | (NS_GREEN | EW_RED);
         DELAY(1); // Delay for ~1 second
@@ -17,16 +25,16 @@ int main() {
         MY_PORT_B = (MY_PORT_B & ~(NS_YELLOW | NS_GREEN | EW_YELLOW | EW_GREEN)) | (NS_RED | EW_RED);
         DELAY(1); // Delay for ~1 second
 
+        // North/South red, East/West red, East/West yellow
+        MY_PORT_B = (MY_PORT_B & ~(NS_YELLOW | NS_GREEN | EW_GREEN)) | (NS_RED | EW_RED | EW_YELLOW);
+        DELAY(1); // Delay for ~1 second
+
         // North/South red, East/West green
         MY_PORT_B = (MY_PORT_B & ~(NS_YELLOW | NS_GREEN | EW_RED | EW_YELLOW)) | (NS_RED | EW_GREEN);
         DELAY(1); // Delay for ~1 second
 
         // North/South red, East/West yellow
         MY_PORT_B = (MY_PORT_B & ~(NS_YELLOW | NS_GREEN | EW_RED | EW_GREEN)) | (NS_RED | EW_YELLOW);
-        DELAY(1); // Delay for ~1 second
-
-        // All red (North/South and East/West)
-        MY_PORT_B = (MY_PORT_B & ~(NS_YELLOW | NS_GREEN | EW_YELLOW | EW_GREEN)) | (NS_RED | EW_RED);
         DELAY(1); // Delay for ~1 second
     }
 
